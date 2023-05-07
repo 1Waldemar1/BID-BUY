a2a02eaafddf4c02686273986c672d6011df1474
1Waldemar1 <dantedmc.5@mail.ru>
2023-05-08 01:11:49 +0300

    add products and footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { NavBar } from "./components/navbar/navbar";
 import { Header } from "./components/header/header";
 import { HomeProducts } from "./components/home-products/home-products";
+import { Footer } from "./components/footer/footer";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <NavBar />
         <HomeProducts />
       </div>
+      <Footer />
     </div>
   );
 }
